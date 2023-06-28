20847b9d3192c772b106b4f52dd720f9ab47f6a9
AlexKhaf <hafisov.a.v@gmail.com>
2023-06-28 22:21:35 +0300

    Added custom_filter. Unit D3.4. task done

---
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -0,0 +1,18 @@
+from django import template
+
+register = template.Library()
+# если мы не зарегестрируем наши фильтры, то django никогда не узнает где именно их искать и фильтры потеряются :(
+
+
+@register.filter(name='censor')
+# регистрируем наш фильтр под именем multiply, чтоб django понимал, что это именно фильтр, а не простая функция
+def censor(value, arg):
+    censor_list = [
+        "ругательство",
+        "оскорбление",
+        "плохое_слово"
+    ]
+    for censor in censor_list:
+        if value.find(censor) != -1:
+            value = value.replace(censor,"***")
+    return value
--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import NewsList, NewsDetail  # импортируем наше представление
 
+
 urlpatterns = [
     # path — означает путь. В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно почему
     path('', NewsList.as_view()),
